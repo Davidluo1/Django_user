7a142f93c6150f4bf42384c71e5715e4de1aa661
David Luo <LuoDavidYi@gmail.com>
2023-11-30 09:05:44 -0500

    Update otp.py

--- user/models/otp.py
+++ user/models/otp.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class OtpUser(models.Model):
     """Otp User"""
-    
     otp_value = models.IntegerField()
     user = models.ForeignKey("user.User", models.DO_NOTHING)
      
